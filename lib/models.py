0f9af7f18fab2566e855978d2bed9ff62886650f
Saki <sbasken@gmail.com>
2023-03-26 12:24:06 -0700

    establish one to many relational database using SQLAlchemy and alembic

--- lib/models.py
+++ lib/models.py
@@ -2,9 +2,43 @@ from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+#Note that we've added a convention and metadata to our models. 
+#SQLite requires names for changes to foreign keys
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
+#Our convention provides a template for naming these changes, and 
+# metadata saves them to a sqlalchemy.MetaData object. 
 
 Base = declarative_base(metadata=metadata)
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+
+    #add the relationship to Game
+    reviews = relationship('Review', backref=backref('game'))
+
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
